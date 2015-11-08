b02638e107270ea8e5ac3cd843797c614e617737
zeksdev <zeljko.bajsanski@gmail.com>
2015-11-08 02:45:27 +0100

    Error codes

--- src/android/BrotherPrinter.java
+++ src/android/BrotherPrinter.java
@@ -36,22 +36,22 @@ public class BrotherPrinter extends CordovaPlugin {
     myPrinterInfo.orientation = PrinterInfo.Orientation.PORTRAIT;
     myPrinterInfo.numberOfCopies = 1;
     myPrinter.setPrinterInfo(myPrinterInfo);
-    //String filePath = "/storage/emulated/0/Digital Editions/racun.pdf";
-    //String filePath = "/storage/emulated/0/Bluetooth/20150116_144555.jpg";
-    //PrinterStatus status = myPrinter.printPDF(filePath, 1);
 
     PrinterStatus status = null;
     try {
         Bitmap image = base64ToBitmap(data);
         status = myPrinter.printImage(image);
-        callbackContext.success(status.errorCode.toString());
+        if (status.errorCode == PrinterStatus.ERROR_NONE) {
+            callbackContext.success();
+        } else {
+            callbackContext.error(status.errorCode.toString());
+        }
     } catch(Exception exc) {
         callbackContext.error(exc.getMessage());
     }
   }
 
-  private Bitmap base64ToBitmap(String imageData)
-    {
+    private Bitmap base64ToBitmap(String imageData) {
         byte[] imageAsBytes = Base64.decode(imageData.getBytes(), Base64.DEFAULT);
         return BitmapFactory.decodeByteArray(imageAsBytes, 0, imageAsBytes.length);
     }
